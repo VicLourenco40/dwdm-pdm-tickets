099318329e296ae28cc64123ea15113beefeb79b
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2024-12-31 14:44:49 +0000

    Create Ticket model

---
+++ app/Models/Ticket.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+
+class Ticket extends Model
+{
+    use HasUuids;
+
+    protected $fillable = ['event_id', 'type'];
+
+    public $timestamps = false;
+}
