5f4f036ee708d81578c2e34e09354ce11744e41e
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2024-12-31 15:06:17 +0000

    Add route for PurchaseController show

--- routes/api.php
+++ routes/api.php
@@ -2,3 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PurchaseController;
+
+Route::get('/tickets/{ticketCode}', [PurchaseController::class, 'show']);
