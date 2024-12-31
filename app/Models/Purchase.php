344b89b0e792a9907bfbd37205b03540d27706f2
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2024-12-31 14:54:55 +0000

    Add model relationships

--- app/Models/Purchase.php
+++ app/Models/Purchase.php
@@ -4,6 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\Ticket;
+use App\Models\Customer;
 
 class Purchase extends Model
 {
@@ -18,4 +21,15 @@ class Purchase extends Model
     ];
 
     public $timestamps = false;
+
+    public function ticket(): BelongsTo
+    {
+        return $this->belongsTo(Ticket::class)
+                    ->with('event');
+    }
+
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class);
+    }
 }
--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\Event;
 
 class Ticket extends Model
 {
@@ -12,4 +14,9 @@ class Ticket extends Model
     protected $fillable = ['event_id', 'type'];
 
     public $timestamps = false;
+
+    public function event(): BelongsTo
+    {
+        return $this->belongsTo(Event::class);
+    }
 }
