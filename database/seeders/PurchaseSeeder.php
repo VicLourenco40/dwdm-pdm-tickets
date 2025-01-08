2d32fb2ea5a55d1d60cc16ec5fa08794ec6152e0
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2025-01-08 19:12:13 +0000

    Remove trailing commas

--- database/seeders/PurchaseSeeder.php
+++ database/seeders/PurchaseSeeder.php
@@ -16,21 +16,21 @@ class PurchaseSeeder extends Seeder
                 'ticket_id' => 'e09d0bfe-3751-494b-959f-44ff17b8c9d6',
                 'customer_id' => 'b8a4ed28-d65d-4422-b05d-a78fa945d415',
                 'ticket_code' => '01jgepy60fxhghnaxz3z0v11qq',
-                'purchased_at' => '2024-09-01 16:29:36',
+                'purchased_at' => '2024-09-01 16:29:36'
             ],
             [
                 'id' => 'd78ca3dc-f23b-4859-9b5e-3f3bc8810848',
                 'ticket_id' => 'e09d0bfe-3751-494b-959f-44ff17b8c9d6',
                 'customer_id' => '3f660672-ef62-493c-9dc2-2d477f893734',
                 'ticket_code' => '01jgepy60fth830j8h2pywqjp1',
-                'purchased_at' => '2024-08-22 13:43:21',
+                'purchased_at' => '2024-08-22 13:43:21'
             ],
             [
                 'id' => 'bfaf2e6e-5d10-4caf-a2dc-063ea9b04087',
                 'ticket_id' => '0218ac84-3edf-4157-89b7-3252fb89a6e7',
                 'customer_id' => '623da2c4-82dc-4f77-ab1f-f3604215dbde',
                 'ticket_code' => '01jgepy60fyjga56nr8bd5w61g',
-                'purchased_at' => '2024-09-04 18:55:08',
+                'purchased_at' => '2024-09-04 18:55:08'
             ]
         ];
 
